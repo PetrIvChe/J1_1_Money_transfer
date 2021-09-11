ca73f8acf6ff9607ee102451fc9303d6894329c2
PetrIvChe <cherniavskiipetr@gmail.com>
2021-09-11 15:51:50 +0300

    End of work

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
         public static void main(String[] args) {
-            int balance = 2_147_483_647;
+            int balance = 2_000_000_000;
             int replenishment = 500_000_000;
             int total = balance + replenishment;
             System.out.println(total);
